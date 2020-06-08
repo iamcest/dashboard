c02434e45531328085c87ed2330da15cce0a91d3
iamcest <44790773+iamcest@users.noreply.github.com>
2020-06-07 21:24:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 // app.js
 let auth0 = null;
 const fetchAuthConfig = () => fetch("/auth_config.json");
-
-const configureClient = async () => {
-  const response = await fetchAuthConfig();
-  const config = await response.json();
-
-  auth0 = await createAuth0Client({
-    domain: config.domain,
-    client_id: config.clientId
-  });
-};
 var userInViews = require('./lib/middleware/userInViews');
 var authRouter = require('./routes/auth');
 var indexRouter = require('./routes/index');
