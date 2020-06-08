0ac2c18ae8c40ff265b2b6c5354edce93c9ed8e1
iamcest <44790773+iamcest@users.noreply.github.com>
2020-06-07 21:31:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,73 +1,37 @@
-// app.js
-let auth0 = null;
-const fetchAuthConfig = () => fetch("/auth_config.json");
-var userInViews = require('./lib/middleware/userInViews');
-var authRouter = require('./routes/auth');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const flash = require('connect-flash');
+const userInViews = require('./lib/middleware/userInViews');
+const authRouter = require('./routes/auth');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-// ..
-app.use(userInViews());
-app.use('/', authRouter);
-app.use('/', indexRouter);
-app.use('/', usersRouter);
-// ..
-
-var session = require('express-session');
-
-// config express-session
-var sess = {
-  secret: 'iamcest-dashboard',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true
-};
-
-if (app.get('env') === 'production') {
-  // Use secure cookies in production (requires SSL/TLS)
-  sess.cookie.secure = true;
-
-  // Uncomment the line below if your application is behind a proxy (like on Heroku)
-  // or if you're encountering the error message:
-  // "Unable to verify authorization request state"
-  // app.set('trust proxy', 1);
-}
-
-app.use(session(sess));
-
-// app.js
-
-// Load environment variables from .env
-var dotenv = require('dotenv');
 dotenv.config();
 
-// Load Passport
-var passport = require('passport');
-var Auth0Strategy = require('passport-auth0');
-
 // Configure Passport to use Auth0
-var strategy = new Auth0Strategy(
+const strategy = new Auth0Strategy(
   {
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback',
   },
   function (accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
     // profile has all the information from the user
     return done(null, profile);
-  }
+  },
 );
 
 passport.use(strategy);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// app.js
 
 // You can use this section to keep a smaller payload
 passport.serializeUser(function (user, done) {
@@ -78,4 +42,81 @@ passport.deserializeUser(function (user, done) {
   done(null, user);
 });
 
+const app = express();
 
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(cookieParser());
+
+// config express-session
+const sess = {
+  secret: process.env.EXPRESS_SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+};
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1);
+  sess.cookie.secure = true; // serve secure cookies, requires https
+}
+
+app.use(session(sess));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
+
+// Handle auth failure error messages
+app.use(function (req, res, next) {
+  if (req && req.query && req.query.error) {
+    req.flash('error', req.query.error);
+  }
+  if (req && req.query && req.query.error_description) {
+    req.flash('error_description', req.query.error_description);
+  }
+  next();
+});
+
+app.use(userInViews());
+app.use('/', authRouter);
+app.use('/', indexRouter);
+app.use('/', usersRouter);
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handlers
+
+// Development error handler
+// Will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
+    });
+  });
+}
+
+// Production error handler
+// No stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
+});
+
+module.exports = app;
